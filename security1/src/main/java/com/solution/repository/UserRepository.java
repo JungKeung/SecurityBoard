d59de47841e4bc61e229a074056f1727682a4cc3
jungkeungLee <jungkeung0117@gmail.com>
2021-12-25 19:30:29 +0900

    해당파일 주석 수정

--- security1/src/main/java/com/solution/repository/UserRepository.java
+++ security1/src/main/java/com/solution/repository/UserRepository.java
@@ -3,9 +3,7 @@ package com.solution.repository;
 import com.solution.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-//CRUD 함수를 JpaRepository가 들고 있음.
-// @Repository라는 어노테이션이 없어도 Ioc된다. 이유는 JpaRepository를 상속했기 때문에
+// JpaRepository 를 상속했으므로 해당 객체가 자동으로 빈에 등록된다.
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     //findBy규칙 -> username문법
