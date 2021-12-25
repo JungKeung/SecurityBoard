0804c9cbcc8d565e919b3585fd464a285b6f59ab
jungkeungLee <jungkeung0117@gmail.com>
2021-12-25 21:30:36 +0900

    회원가입 > 약관동의 기능 추가에 따른 User Model 의 프로퍼티 추가 (엔티티 모델이여서 컬럼도 함께 추가됨)

--- security1/src/main/java/com/solution/model/User.java
+++ security1/src/main/java/com/solution/model/User.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
 import javax.persistence.*;
 
 @ToString
@@ -14,14 +13,34 @@ import javax.persistence.*;
 public class User {
 
     @Id
+    /**
+     * 유저 이메일
+     */
     private String email;
 
+    /**
+     * 유저 패스워드
+     */
     private String password;
+
+    /**
+     * 유저 닉네임
+     */
     private String nickname; //작성자
-    private String role; //ROLE_USER,ROLE_ADMIN
-    //약관 동의
-    private Long consent;
-    private Long disagree;
 
+    /**
+     * 유저 권한 (sequrity)
+     * ROLE_USER,ROLE_ADMIN
+     */
+    private String role;
 
+    /**
+     * 개인정보 이용약관 (필수)
+     */
+    private boolean isAgreePrivacyTerms;
+
+    /**
+     * 마케팅 이용약관 (선택)
+     */
+    private boolean isAgreeMarketingTerms;
 }
