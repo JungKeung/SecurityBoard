706f4f4ae583fead1a458452eaace386c303d624
jungkeungLee <jungkeung0117@gmail.com>
2021-12-27 00:37:01 +0900

    boardService 객체를 타지 않고, boardRepository 로 바로 호출될 수 있도록 수정됨에 따라 기존 메서드 삭제

--- security1/src/main/java/com/solution/service/BoardService.java
+++ security1/src/main/java/com/solution/service/BoardService.java
@@ -32,18 +32,13 @@ public class BoardService {
 
     // 게시글 수정 시 특정 Id 가져오기
     public Long update(Board board) {
-        boardRepository.save ( board );
-        return board.getId ();
+        Board newBoard = boardRepository.save ( board );
+        return newBoard.getId ();
     }
 
     //게시글 목록 가져오기
     public List<Board> findBoards() {
         return boardRepository.findAll();
-    }
-
-    //게시글 화면 보여주기
-    public Board boardDetail (Long id) {
-        return boardRepository.findById(id).get();
     }
 
 }
