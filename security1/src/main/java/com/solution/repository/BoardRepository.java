7261a275acb8e931c5e3610bf08d49e5bb282a2e
jungkeungLee <jungkeung0117@gmail.com>
2021-12-26 18:25:32 +0900

    기존 PagingRepository.java 와 BoardRepository.java 가 중복 되어 BoardRepository.java 로 통합

--- security1/src/main/java/com/solution/repository/BoardRepository.java
+++ security1/src/main/java/com/solution/repository/BoardRepository.java
@@ -1,36 +1,17 @@
 package com.solution.repository;
 
 import com.solution.model.Board;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 
-@Repository
-@RequiredArgsConstructor
-public class BoardRepository {
 
-    private final EntityManager em;
+public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    //게시글 저장
-    public Board save(Board board){
-        em.persist (board);
-        return board;
-    }
+    @Override
+    List<Board> findAll();
 
-    // 게시글 리스트 가져오기
-    public List<Board> findAll(){
-        return em.createQuery ( "select m from Board m", Board.class )
-                .getResultList ();
-    }
-
-    //특정 회원의 이름으로 찾기기
-    public List<Board> findByName(String name){
-        return em.createQuery ( "select m from Board m where m.name = :name", Board.class)
-                .setParameter ( "name",name )
-                .getResultList ();
-
-    }
+    Page<Board> findByTitleContaining(String searchKeyword, Pageable pageable);
 }
-
