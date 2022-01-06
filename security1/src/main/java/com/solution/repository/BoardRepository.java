fb7852aa9f9e4ef3415fca24f9c631a16039d724
jungkeungLee <jungkeung0117@gmail.com>
2022-01-06 16:48:31 +0900

    게시판 검색 기능 추가(제목, 내용, 작성자, 내용+제목) 코드 추가

--- security1/src/main/java/com/solution/repository/BoardRepository.java
+++ security1/src/main/java/com/solution/repository/BoardRepository.java
@@ -4,7 +4,6 @@ import com.solution.model.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 
@@ -14,4 +13,9 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     List<Board> findAll();
 
     Page<Board> findByTitleContaining(String searchKeyword, Pageable pageable);
+
+    Page<Board> findByContentContaining(String searchKeyword, Pageable pageable);
+
+    Page<Board> findByUserEmailContaining(String searchKeyword, Pageable pageable);
+
 }
